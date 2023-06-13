c5bde2555d2b8c76248a0ca7d6f94990f5c11129
rohanparikh <parikhrs@mail.uc.edu>
2023-06-12 21:22:18 -0400

    Basic Working Code

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+
+        double creditCardBalance = 5000.0;
+        final double INTEREST_RATE = 0.17;
+        double oneMonthInterest = 0.0;
+        double twoMonthInterest = 0.0;
+
+        oneMonthInterest = creditCardBalance * INTEREST_RATE;
+        twoMonthInterest = (creditCardBalance + oneMonthInterest) * INTEREST_RATE;
+
+        // Rounds answer
+        oneMonthInterest = Math.round(oneMonthInterest * 100) / 100.0;
+        twoMonthInterest = Math.round(twoMonthInterest * 100) / 100.0;
+
+
+        System.out.println("The interest due after one month is $" + oneMonthInterest);
+        System.out.println("The interest due after two months is $" + twoMonthInterest);
+
+
     }
 }
